3c2fcbbde8dcef709e9fa072fb0edb34d8c18c9f
thiagocruzrj <thiago.justo@al.infnet.edu.br>
2019-12-08 00:17:15 -0300

    Removing front-end project from monolite

--- backend-scm/config/keys-env.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  secretOrKey: process.env.SECRET_OR_KEY,
-  clientId:  process.env.GITHUB_CLIENT_ID,
-  clientSecret:  process.env.GITHUB_CLIENT_SECRET
-};
--- backend-user/read-package-version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-PACKAGE_VERSION=$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
-echo $PACKAGE_VERSION
